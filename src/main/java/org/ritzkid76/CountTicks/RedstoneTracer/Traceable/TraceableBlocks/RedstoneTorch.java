50174770145dfb5c0964f0ee5cd7ba61350fa284
RitzKid76 <tjbaker2003@hotmail.com>
2024-11-01 18:28:34 -0400

    add RedstoneTorch

---
+++ src/main/java/org/ritzkid76/CountTicks/RedstoneTracer/Traceable/TraceableBlocks/RedstoneTorch.java
@@ -0,0 +1,32 @@
+package org.ritzkid76.CountTicks.RedstoneTracer.Traceable.TraceableBlocks;
+
+import com.sk89q.worldedit.math.BlockVector3;
+import org.bukkit.block.data.BlockData;
+import org.ritzkid76.CountTicks.RedstoneTracer.GameTickDelay;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.Connection.Connection;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.Connection.ConnectionDirection;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.Connection.ConnectionSetFactory;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.Connection.PowerType;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.Traceable;
+import org.ritzkid76.CountTicks.RedstoneTracer.Traceable.TraceableBlockData;
+
+import java.util.Set;
+
+public class RedstoneTorch extends Traceable {
+    public static final Set<Connection> inputs = new ConnectionSetFactory()
+        .add(ConnectionDirection.UP, PowerType.ANY)
+    .get();
+    public static final Set<Connection> outputs = new ConnectionSetFactory()
+        .add(ConnectionDirection.CARDINAL, PowerType.SOFT)
+        .add(ConnectionDirection.UP, PowerType.HARD)
+    .get();
+
+    public RedstoneTorch(BlockData data, BlockVector3 position) {
+        super(inputs, outputs, data, position);
+    }
+
+    @Override
+    public TraceableBlockData applyBlockData(BlockData blockData) {
+        return new TraceableBlockData(new GameTickDelay(2));
+    }
+}
