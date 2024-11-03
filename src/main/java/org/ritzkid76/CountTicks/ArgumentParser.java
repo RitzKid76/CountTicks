d578cbf2a052331abb15a0d5b923f465ca0812b0
RitzKid76 <tjbaker2003@hotmail.com>
2024-11-03 17:15:46 -0500

    stash for revert

---
+++ src/main/java/org/ritzkid76/CountTicks/ArgumentParser.java
@@ -0,0 +1,8 @@
+package org.ritzkid76.CountTicks;
+
+import org.ritzkid76.CountTicks.TabCompletion.SyntaxHandler;
+
+public class ArgumentParser {
+    private SyntaxHandler syntaxHandler;
+    public ArgumentParser(SyntaxHandler handler) { syntaxHandler = handler; }
+}
