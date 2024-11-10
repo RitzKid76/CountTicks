9132abf2b67dcdb10cdb7af17b54898d96539001
RitzKid76 <tjbaker2003@hotmail.com>
2024-11-10 00:34:00 -0500

    remove questionable comment

--- src/main/java/org/ritzkid76/CountTicks/RedstoneTracer/Traceable/Traceable.java
+++ src/main/java/org/ritzkid76/CountTicks/RedstoneTracer/Traceable/Traceable.java
@@ -21,7 +21,6 @@ public abstract class Traceable {
 
 	public final World world;
 
-	// some retard shit since java doesnt let me call super() unless its the first method in the function. makes sense, but sadge.
 	public Traceable(Set<Connection> in, Set<Connection> out, BlockData blockData, BlockVector3 pos, World wld) {
 		TraceableBlockData data = applyBlockData(blockData);
 		BlockFace direction = data.direction();
